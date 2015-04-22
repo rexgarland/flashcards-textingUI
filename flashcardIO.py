761bd9095150c41f378ced4a6f722f1f19a8445a
rexgarland <rex.garland@gmail.com>
2015-04-22 02:58:42 -0700

    cleaned card id handling with flashcard IO

--- flashcardIO.py
+++ flashcardIO.py
@@ -9,21 +9,24 @@ A module for calculating properties on flashcards.
 	- includes counts
 
 3) a metric for necessity
-	- includes difficulty and neglectedness
+	- includes difficulty and days_old
 
 4) a method for rewriting a data file
 
 """
+import selection
 import numpy as np
 import os, pickle
 
+METADATAFILE = 'metadata.dat'
+
 class Flashcard(object):
 	def __init__(self, data):
-		self.text = tuple(data[:2])
-		self.correct_count = data[2]
-		self.incorrect_count = data[3]
-		self.neglectedness = data[4]
-		self.id = data[5]
+		self.text = data['text']
+		self.correct_count = data['correct_count']
+		self.incorrect_count = data['incorrect_count']
+		self.days_old = data['days_old']
+		self.id = data['id']
 
 	def difficulty(self):
 		# returns difficulty as a result of correct/incorrect
@@ -33,107 +36,150 @@ class Flashcard(object):
 
 	def necessity(self):
 		# updates necessity by including difficulty and neglectedness
-		return (1-np.exp(-float(self.neglectedness)))*(1-np.exp(-float(self.difficulty())))
+		return (1-np.exp(-float(self.days_old)))*(1-np.exp(-float(self.difficulty())))
 
-	def to_text(self):
+	def metadata(self):
+		return (self.correct_count, self.incorrect_count, self.days_old)
+
+	def message(self):
 		# creates text to display when sending card to user
 		return self.text[0]+'\n'*4+self.text[1]
 
 	def __str__(self):
-		return self.text[0]+'\t'+self.text[1]+'\t'+str(self.correct_count)+'\t'+str(self.incorrect_count)+'\t'+str(self.neglectedness)+'\t'+str(self.id)
+		return self.text[0]+'\t'+self.text[1]+'\t'+str(self.id)
 
-def new_id():
-	with open('ids.dat','r') as f:
-		ids = pickle.load(f)
-	rando = np.random.randint(10000)
-	while rando in ids:
-		rando = np.random.randint(10000)
-	with open('ids.dat','w') as f:
-		pickle.dump(ids+[rando], f)
-	return rando
+def loadmetadata():
+	metadata = {}
+	if os.path.isfile(METADATAFILE):
+		with open(METADATAFILE, 'r') as f:
+			metadata = pickle.load(f)
+	return metadata
 
-def line_to_flashcard(line):
+def new_id(metadata):
+	# assign next biggest id number
+	if not metadata:
+		cardid = 0
+	else:
+		cardid = max(metadata)+1
+	return cardid
+
+def line_to_flashcard(line, metadata):
 	split = line.strip().split('\t')
-	if len(split)==2 or len(split)==5:
-		rando = new_id()
-		flash = split[:]+[0,0,10000,rando]
-	elif len(split)==6:
-		flash = split[:2]+[int(split[2])]+[int(split[3])]+[float(split[4])]+[int(split[5])]
+	card = {}
+	if len(split)==2 or len(split)>3:
+		card['text'] = tuple(split)
+		card['correct_count'] = 0
+		card['incorrect_count'] = 0
+		card['days_old'] = 0
+		card['id'] = new_id(metadata)
+		metadata[card['id']] = (card['correct_count'], card['incorrect_count'], card['days_old'])
+	elif len(split)==3:
+		card['text'] = tuple(split[:2])
+		card['id'] = int(split[2])
+		card['correct_count'], card['incorrect_count'], card['days_old'] = metadata[card['id']]
 	else:
 		raise Exception
-	return Flashcard(flash)
+	return Flashcard(card), metadata
 
 def load(datafile):
 	# returns a list of flashcard instances
-	f = open(datafile, 'r')
-	flashcards = []
-	line_no = 0
-
-	while True:
-		try:
-			line_no += 1
-			line = f.next()
-			if line[0]=='#' or line=='\n':
-				continue
-			flashcards.append(line_to_flashcard(line))
-		except StopIteration:
-			break
-		except ValueError:
-			print "Error: could not convert line %d of reading file %s to flashcard data." % (line_no, datafile)
-			break
-		except:
-			print "Error: unexpected error at line %d of reading file %s." % (line_no, datafile)
-			break
-
-	f.close()
-	return flashcards
-
-def write(datafile, flashcard_list):
 	buffer_file = open('buffer.txt','w')
 	read_file = open(datafile, 'r')
-	status = True
+	flashcards_dict = {}
 
-	list_of_firsts = [flashcard.text[0].strip() for flashcard in flashcard_list]
+	metadata = loadmetadata()
 
-	while True:
+	change = False
+	lines = read_file.readlines()
+	line_no = 0
+	for line in lines:
+		line_no += 1
+		if line[0]=='#':
+			buffer_file.write(line)
+			continue
+		if line=='\n':
+			continue
 		try:
-			line = read_file.next()
-			if line[0]=='#' or line=='\n':
-				buffer_file.write(line)
-				continue
-			first = line.split('\t')[0].strip()
-			try:
-				current_card = flashcard_list[list_of_firsts.index(first)]
-			except:
-				print "Error: could not match current file with loaded flashcards. Instance: %s" % first
-				status = False
-				break
-			buffer_file.write(str(current_card)+'\n')
-		except StopIteration:
-			break
+			current_card, metadata = line_to_flashcard(line, metadata)
 		except:
-			status = False
-			print "Error: unexpected error. File reading/writing stopped. File: " + __file__
-			break
+			"Error: could not convert line %d of file '%s' to flashcard. Flashcard ignored." % (line_no, datafile)
+			buffer_file.write(line)
+			continue
+		change = True
+		flashcards_dict[current_card.id] = current_card
+		buffer_file.write(str(current_card)+'\n')
 
-	buffer_file.close()
 	read_file.close()
+	buffer_file.close()
 
-	if status:
+	with open(METADATAFILE, 'w') as metafile:
+		pickle.dump(metadata, metafile)
+
+	if change:
 		os.remove(datafile)
 		os.rename('buffer.txt', datafile)
+	else:
+		os.remove('buffer.txt')
 
-def update_cards(updates_dict):
-	# updates_dict is a dict of flashcard ids and corresponding "correct" booleans
-	ids = update_dict.keys()
+	return flashcards_dict.values()
+
+def write_updated(flashcards_dict):
+	metadata = loadmetadata()
+
+	for cardid in flashcards_dict:
+		metadata[cardid] = flashcards_dict[cardid].metadata()
+
+	with open(METADATAFILE, 'w') as metafile:
+		pickle.dump(metadata, metafile)
+
+def clean_metadata():
+	ids = []
 	for filename in selection.find_files():
-		cards = flashcardIO.load(filename)
-		for card in cards:
-			if card.id in ids:
+		ids.extend([flashcard.id for flashcard in load(filename)])
+	metadata = loadmetadata()
+	new_metadata = {}
+	for id in ids:
+		if id in metadata:
+			new_metadata[id] = metadata[id]
+	with open(METADATAFILE, 'w') as metafile:
+		pickle.dump(new_metadata, metafile)
+
+def clear_metadata(filename):
+	metadata = loadmetadata()
+	with open(filename, 'r') as f:
+		lines = f.readlines()
+	buffer_file = open('buffer.txt','w')
+	line_no = 0
+	for line in lines:
+		line_no += 1
+		split = line.strip().split('\t')
+		if len(split)==3:
+			try:
+				del metadata[int(split[2])]
+				buffer_file.write(split[0]+'\t'+split[1]+'\n')
+			except:
+				print "Error: could not read id from line %d in file '%s'." % (line_no, filename)
+		else:
+			buffer_file.write(line)
+	buffer_file.close()
+	with open(METADATAFILE, 'w') as metafile:
+		pickle.dump(metadata, metafile)
+	os.remove(filename)
+	os.rename('buffer.txt', filename)
+	clean_metadata()
+
+def daily_update(updates_dict):
+	"""updates_dict is a dict of flashcard ids and corresponding "correct" 
+	booleans"""
+	for filename in selection.find_files():
+		cards = {}
+		for card in load(filename):
+			if card.id in updates_dict:
 				card.correct_count +=  int(updates_dict[card.id])
 				card.incorrect_count +=  int(not updates_dict[card.id])
-				card.neglectedness = 0
-				del updates_dict[card.id]
+				card.days_old = 0
+				cards[card.id] = card
 			else:
-				card.neglectedness += 1
-		write(filename, cards)
+				card.days_old += 1
+				cards[card.id] = card
+		write_updated(cards)
